27add6cec3e00aed32eecaa160252ccb251700c1
Antonio Rodrigues Coutinho <antoniorodrigues.coutinho@gmail.com>
2019-08-30 16:28:07 +0100

    Refactoring, add before and after anotation.

--- src/main/java/CTMobileTest.java
+++ src/main/java/CTMobileTest.java
@@ -2,7 +2,9 @@ import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -15,53 +17,26 @@ import java.util.concurrent.TimeUnit;
 
 public class CTMobileTest {
 
+    private AndroidDriver<MobileElement> driver;
 
-    @Test
-    public void testInputName() throws MalformedURLException{
-
-        AndroidDriver<MobileElement> driver = getMobileElementAndroidDriver();
-
-        //Select form
-        //List<MobileElement> elements = driver.findElements(By.xpath("//android.widget.TextView[@text='Formulário']"));
-        MobileElement formBtt =  driver.findElement(By.xpath("//android.widget.TextView[@text='Formulário']"));
-        formBtt.click();
-
-        //Click Combo
-        driver.findElement(MobileBy.AccessibilityId("console")).click();
-
-        //Select desired option
-        driver.findElement(By.xpath("//android.widget.CheckedTextView[@text='PS4']")).click();
-
-        /*
-        String optionSelected = driver.findElement(MobileBy.AccessibilityId("console")).findElement(By.className("android.widget.TextView")).getText();
-        or
-        String optionSelected = driver.findElement(By.xpath("//android.widget.Spinner/android.widget.TextView")).getText();
-        */
-
-        String optionSelected = driver.findElement(By.xpath("//android.widget.Spinner/android.widget.TextView")).getText();
-
-        //Check
-        Assert.assertEquals("PS4" , optionSelected);
-        driver.quit();
-
-    }
-
-    private AndroidDriver<MobileElement> getMobileElementAndroidDriver() throws MalformedURLException {
+    @Before
+    public void getMobileElementAndroidDriver() throws MalformedURLException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("platformName", "Android");
         desiredCapabilities.setCapability("deviceName", "emulator-5554");
         desiredCapabilities.setCapability("automationName", "uiautomator2");
         desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\User\\IdeaProjects\\androidtest\\src\\main\\resources\\CTAppium-1-1.apk");
 
-        AndroidDriver<MobileElement> driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
+        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        return driver;
+    }
+    @After
+    public void tearDown(){
+        driver.quit();
     }
 
     @Test
-    public void testComboBox() throws MalformedURLException{
-
-        AndroidDriver<MobileElement> driver = getMobileElementAndroidDriver();
+    public void testInputName() throws MalformedURLException{
 
         //Select form
         //List<MobileElement> elements = driver.findElements(By.xpath("//android.widget.TextView[@text='Formulário']"));
@@ -84,13 +59,38 @@ public class CTMobileTest {
 
         //Check
         Assert.assertEquals("PS4" , optionSelected);
-        driver.quit();
+
+    }
+
+
+    @Test
+    public void testComboBox() throws MalformedURLException{
+
+        //Select form
+        //List<MobileElement> elements = driver.findElements(By.xpath("//android.widget.TextView[@text='Formulário']"));
+        MobileElement formBtt =  driver.findElement(By.xpath("//android.widget.TextView[@text='Formulário']"));
+        formBtt.click();
+
+        //Click Combo
+        driver.findElement(MobileBy.AccessibilityId("console")).click();
+
+        //Select desired option
+        driver.findElement(By.xpath("//android.widget.CheckedTextView[@text='PS4']")).click();
+
+        /*
+        String optionSelected = driver.findElement(MobileBy.AccessibilityId("console")).findElement(By.className("android.widget.TextView")).getText();
+        or
+        String optionSelected = driver.findElement(By.xpath("//android.widget.Spinner/android.widget.TextView")).getText();
+        */
+
+        String optionSelected = driver.findElement(By.xpath("//android.widget.Spinner/android.widget.TextView")).getText();
+
+        //Check
+        Assert.assertEquals("PS4" , optionSelected);
     }
 
     @Test
     public void testCkeckInput() throws MalformedURLException{
-
-        AndroidDriver<MobileElement> driver = getMobileElementAndroidDriver();
 
         //Select form
         //List<MobileElement> elements = driver.findElements(By.xpath("//android.widget.TextView[@text='Formulário']"));
@@ -108,7 +108,5 @@ public class CTMobileTest {
 
         Assert.assertTrue(ch.getAttribute("checked").equals("true"));
         Assert.assertTrue(sw.getAttribute("checked").equals("false"));
-
-        driver.quit();
     }
 }
