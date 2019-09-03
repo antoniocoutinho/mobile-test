dc3c1f7b813db9a7d531687904e4521157ed2c8e
Antonio Rodrigues Coutinho <antoniorodrigues.coutinho@gmail.com>
2019-09-03 11:43:07 +0100

    Add DriverFactory.

--- src/main/java/appium/CTMobileTest.java
+++ src/main/java/appium/CTMobileTest.java
@@ -1,11 +1,11 @@
+package appium;
+
+import core.DriverFactory;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -20,25 +20,37 @@ public class CTMobileTest {
     private AndroidDriver<MobileElement> driver;
 
     @Before
-    public void getMobileElementAndroidDriver() throws MalformedURLException {
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability("platformName", "Android");
-        desiredCapabilities.setCapability("deviceName", "emulator-5554");
-        desiredCapabilities.setCapability("automationName", "uiautomator2");
-        desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\User\\IdeaProjects\\androidtest\\src\\main\\resources\\CTAppium-1-1.apk");
-
-        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    public void getMobileElementAndroidDriver(){
+        driver =DriverFactory.getDriver();
         MobileElement formBtt =  driver.findElement(By.xpath("//android.widget.TextView[@text='Formulário']"));
         formBtt.click();
     }
+
     @After
     public void tearDown(){
-        driver.quit();
+        DriverFactory.getDriver().resetApp();
+    }
+    @AfterClass
+    public static void endClass(){
+        DriverFactory.killDriver();
     }
 
     @Test
-    public void testInputName() throws MalformedURLException{
+    public void testInputName(){
+
+        //Select desired option
+        driver.findElement(MobileBy.AccessibilityId("nome")).sendKeys("Antonio");
+
+        String inputName = driver.findElement(MobileBy.AccessibilityId("nome")).getText();
+
+        //Check
+        Assert.assertEquals("Antonio" , inputName );
+
+    }
+
+
+    @Test
+    public void testComboBox() {
 
         //Click Combo
         driver.findElement(MobileBy.AccessibilityId("console")).click();
@@ -56,38 +68,15 @@ public class CTMobileTest {
 
         //Check
         Assert.assertEquals("PS4" , optionSelected);
-
-    }
-
-
-    @Test
-    public void testComboBox() throws MalformedURLException{
-
-        //Click Combo
-        driver.findElement(MobileBy.AccessibilityId("console")).click();
-
-        //Select desired option
-        driver.findElement(By.xpath("//android.widget.CheckedTextView[@text='PS4']")).click();
-
-        /*
-        String optionSelected = driver.findElement(MobileBy.AccessibilityId("console")).findElement(By.className("android.widget.TextView")).getText();
-        or
-        String optionSelected = driver.findElement(By.xpath("//android.widget.Spinner/android.widget.TextView")).getText();
-        */
-
-        String optionSelected = driver.findElement(By.xpath("//android.widget.Spinner/android.widget.TextView")).getText();
-
-        //Check
-        Assert.assertEquals("PS4" , optionSelected);
     }
 
     @Test
-    public void testCkeckInput() throws MalformedURLException{
+    public void testCkeckInput(){
 
         //Verify Elements Status
         MobileElement ch = driver.findElement(MobileBy.AccessibilityId("check"));
         MobileElement sw = driver.findElement(MobileBy.AccessibilityId("switch"));
-    
+
         //Click on Elements
         ch.click();
         sw.click();
---
+++ src/main/java/core/DriverFactory.java
@@ -0,0 +1,46 @@
+package core;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class DriverFactory {
+
+    private static AndroidDriver<MobileElement> driver;
+
+    public static AndroidDriver<MobileElement> getDriver(){
+        if(driver==null){
+            createDriver();
+        }
+        return driver;
+    }
+
+    public static void killDriver(){
+        if(driver != null){
+            driver.quit();
+            driver = null;
+        }
+    }
+
+    private static void createDriver() {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("deviceName", "emulator-5554");
+        desiredCapabilities.setCapability("automationName", "uiautomator2");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\User\\IdeaProjects\\androidtest\\src\\main\\resources\\CTAppium-1-1.apk");
+
+        try {
+            driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+
+    }
+}
